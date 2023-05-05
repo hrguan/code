3ec8a44a43cd6352881868b91f2c92c337286e0a
hrguan <huiru.guan@outlook.com>
2023-05-05 14:37:51 -0700

    5/5

--- test.py
+++ test.py
@@ -18,3 +18,32 @@ class Solution(object):
             else:
                 right -= 1
         return res
+    
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        left = 0
+        right = len(height)-1
+        left_max = 0
+        right_max = 0
+        res = 0
+        while left < right:
+            if height[left] <= height[right]:
+                #go left
+                if height[left] > left_max:
+                    left_max = height[left]
+                else:
+                    res += (left_max-height[left])
+                left += 1
+            else:
+                #go right
+                if height[right] > right_max:
+                    right_max = height[right]
+                else:
+                    res += (right_max-height[right])
+                right -= 1
+
+
+        return res
