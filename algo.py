ca8a89226a735ca75bf250829a7033f80b6262af
hrguan <huiru.guan@outlook.com>
2023-05-07 11:16:10 -0700

    5/7

--- algo.py
+++ algo.py
@@ -411,3 +411,27 @@ class Solution(object):
                 b -= 1
                 
             last -= 1
+
+    def partition(self, head, x):
+        """
+        :type head: ListNode
+        :type x: int
+        :rtype: ListNode
+        """
+        before_head = ListNode(0)
+        before = before_head
+        after_head = ListNode(0)
+        after = after_head
+
+        while head:
+            if head.val < x:
+                before.next = head
+                before = before.next
+            else:
+                after.next = head
+                after = after.next
+            head = head.next
+        
+        after.next = None
+        before.next = after_head.next
+        return before_head.next
