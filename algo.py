9c7d39b781e6de541d1b4dd1fb32b013768743b7
hrguan <huiru.guan@outlook.com>
2023-05-07 11:00:31 -0700

    5/7

--- algo.py
+++ algo.py
@@ -388,3 +388,26 @@ class Solution(object):
             fast = fast.next
             slow = slow.next
         return True
+    
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        last = m+n-1
+        a = m-1
+        b = n-1
+
+        while b >= 0:
+            if a >= 0 and nums1[a] > nums2[b]:
+                nums1[last] = nums1[a]
+                a -= 1
+                
+            else:
+                nums1[last] = nums2[b]
+                b -= 1
+                
+            last -= 1
