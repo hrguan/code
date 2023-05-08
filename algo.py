99373d31cb5aa1544ba3a1e610eb66c40fb490c8
hrguan <huiru.guan@outlook.com>
2023-05-08 14:08:48 -0700

    5/8

--- algo.py
+++ algo.py
@@ -567,3 +567,17 @@ class Solution(object):
                 start = end+1
         
         return res
+    
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        res = 0 
+        buy = prices[0]
+        for i in range(1, len(prices)):
+            if prices[i] < buy:
+                buy = prices[i]
+            else:
+                res = max(res, prices[i]-buy)
+        return res
