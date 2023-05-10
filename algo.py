7a1ec0bad74106a618b87c51618e391f9f301814
hrguan <huiru.guan@outlook.com>
2023-05-10 09:36:54 -0700

    5/10

--- algo.py
+++ algo.py
@@ -693,3 +693,21 @@ class Solution(object):
                     if temp >= target:
                         res = min(res, right-left+1)
         return res
+
+    def maxSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        q = collections.deque()
+        res = []
+        for i in range(len(nums)):
+            while q and nums[q[-1]] <= nums[i]:
+                q.pop()
+            q.append(i)
+            if q[0] == i-k:
+                q.popleft()
+            if i >= k-1:
+                res.append(nums[q[0]])
+        return res
