d18b5265b84ac172a6dbd69154697df64c727d06
hrguan <huiru.guan@outlook.com>
2023-05-10 14:45:20 -0700

    5/10

--- algo.py
+++ algo.py
@@ -800,3 +800,23 @@ class Solution(object):
                 temp = 1
             res = max(res, temp)
         return res
+
+    def canAttendMeetings(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: bool
+        """
+        if not intervals:
+            return True
+        intervals.sort(key=lambda x:x[0])
+        end = intervals[0][1]
+        q = Queue.PriorityQueue()
+        q.put(end)
+        for i in range(1, len(intervals)):
+            prev_end = q.get()
+            curr_start = intervals[i][0]
+            curr_end = intervals[i][1]
+            if curr_start < prev_end:
+                q.put(prev_end)
+            q.put(curr_end)
+        return True if q.qsize() == 1 else False
