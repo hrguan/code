04c00ce40457400d4f0b6b3b7ddbe799af485c9f
hrguan <huiru.guan@outlook.com>
2023-05-11 14:31:06 -0700

    5/11

--- algo.py
+++ algo.py
@@ -1125,3 +1125,36 @@ class Solution(object):
             curr.next = ListNode(arr[i])
             curr = curr.next
         return dummy.next
+
+    def reverseEvenLengthGroups(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        group = 2
+        tail = head 
+        while tail and tail.next:
+            cnt = 1 
+            cur = tail.next
+            while cur.next and cnt < group:
+                cur = cur.next
+                cnt += 1
+            pre, cur = tail, tail.next
+            if cnt % 2 == 0: 
+                while cnt and cur:
+                    nxt = cur.next
+                    cur.next = pre
+                    pre = cur
+                    cur = nxt
+                    cnt -= 1
+                first = tail.next
+                first.next = cur
+                tail.next = pre
+                tail = first
+            else:
+                while cnt and cur:
+                    pre, cur = cur, cur.next
+                    cnt -= 1
+                tail = pre
+            group += 1
+        return head
