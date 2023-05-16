f6a2aed4bf133074bff6ca4d024dde9d0acbc80a
hrguan <huiru.guan@outlook.com>
2023-05-16 10:29:21 -0700

    5/16

--- algo.py
+++ algo.py
@@ -1543,3 +1543,16 @@ class Solution(object):
             return res
 
         return atMost(nums, k) - atMost(nums, k-1)
+
+    def sortArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        heap = []
+        res = []
+        for num in nums:
+            heapq.heappush(heap, num)
+        while heap:
+            res.append(heapq.heappop(heap))
+        return res
