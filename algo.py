57ae9a309a95e5a1b24f25951071a5ce1405d082
hrguan <huiru.guan@outlook.com>
2023-05-18 10:43:28 -0700

    5/18

--- algo.py
+++ algo.py
@@ -1883,5 +1883,23 @@ class Solution(object):
             
         return res
 
+    def tribonacci(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 0:
+            return 0
+        if n <= 2:
+            return 1
+        n0 = 0
+        n1 = 1
+        n2 = 1
+        res = 0
+        for i in range(3, n+1):
+            res = n0+n1+n2
+            n0, n1, n2 = n1, n2, res
+        return res
+
 
     
