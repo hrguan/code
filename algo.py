8f756704f0064ffd60b1f23ff186f2fe2d5ac79f
hrguan <huiru.guan@outlook.com>
2023-05-18 11:36:50 -0700

    5/18

--- algo.py
+++ algo.py
@@ -1917,5 +1917,18 @@ class Solution(object):
                 dp[i] = dp[i] or dp[i-num]
         return dp[half]
 
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        dp = [float("inf")] * (amount+1)
+        dp[0] = 0
+        for coin in coins:
+            for i in range(coin, amount+1):
+                dp[i] = min(dp[i], dp[i-coin]+1)
+        return -1 if dp[amount] == float("inf") else dp[amount]
+
 
     
