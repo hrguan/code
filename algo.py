6b44434b3bec3afa8c3c4f2ce00283f31640c663
hrguan <huiru.guan@outlook.com>
2023-05-19 10:56:53 -0700

    5/19

--- algo.py
+++ algo.py
@@ -2060,3 +2060,30 @@ class Solution(object):
                 count += 1
 
         return res
+
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        for paren in s:
+            if paren == "(" or paren == "[" or paren == "{":
+                stack.append(paren)
+            elif paren == ")":
+                if stack and stack[-1] == "(":
+                    stack.pop()
+                else:
+                    return False
+            elif paren == "]": 
+                if stack and stack[-1] == "[":
+                    stack.pop()
+                else:
+                    return False
+            elif paren == "}":
+                if stack and stack[-1] == "{":
+                    stack.pop()
+                else:
+                    return False
+        
+        return len(stack) == 0
