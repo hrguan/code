f4426836aa531f47dd4a30928591390b634716aa
hrguan <huiru.guan@outlook.com>
2023-05-19 09:00:23 -0700

    5/19

--- algo.py
+++ algo.py
@@ -1968,3 +1968,18 @@ class Solution(object):
                 return start
         return start
     
+    def findKthPositive(self, arr, k):
+        """
+        :type arr: List[int]
+        :type k: int
+        :rtype: int
+        """
+        l = 0
+        r = len(arr)-1
+        while l <= r:
+            m = (l+r)//2
+            if arr[m]-m-1 < k:
+                l = m+1
+            else:
+                r = m-1
+        return l+k
