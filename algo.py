44acc19199ca9e8623f9921c4ec83b2490d37ccb
hrguan <huiru.guan@outlook.com>
2023-06-07 22:33:10 -0700

    6/7

--- algo.py
+++ algo.py
@@ -2275,4 +2275,37 @@ class Solution(object):
             s = temp
         return res -1
 
-        
+        #https://www.geeksforgeeks.org/number-of-distinct-words-of-size-n-with-at-most-k-contiguous-vowels/
+    def power(x, y, p):
+        res = 1
+        x = x % p
+        if (x == 0):
+            return 0
+        while (y > 0):
+            if (y & 1):
+                res = (res * x) % p
+            y = y >> 1
+            x = (x * x) % p
+        return res
+ 
+    def kvowelwords(N, K):
+        i, j = 0, 0
+        MOD = 1000000007
+        dp = [[0 for i in range(K + 1)] for i in range(N + 1)]
+        sum = 1
+        for i in range(1, N + 1):
+            dp[i][0] = sum * 21
+            dp[i][0] %= MOD
+            sum = dp[i][0]
+            for j in range(1, K + 1):
+                if (j > i):
+                    dp[i][j] = 0
+                elif (j == i):
+                    dp[i][j] = power(5, i, MOD)
+                else:
+                    dp[i][j] = dp[i - 1][j - 1] * 5
+                dp[i][j] %= MOD
+                sum += dp[i][j]
+                sum %= MOD
+    
+        return sum
