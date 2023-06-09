71b8fe8dd8f4b446adef0aa6f4c7f175564b0965
hrguan <huiru.guan@outlook.com>
2023-06-09 15:14:03 -0700

    6/9

--- algo.py
+++ algo.py
@@ -2322,3 +2322,16 @@ class Solution(object):
                 sum %= MOD
     
         return sum
+
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        def dfs(nums, index, path, res):
+            res.append(path)
+            for i in range(index, len(nums)):
+                dfs(nums, i+1, path+[nums[i]], res)
+        res = []
+        dfs(sorted(nums), 0, [], res)
+        return res
