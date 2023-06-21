1dcfb84477e7c06659d16fcac1d6613abcf9c82d
hrguan <huiru.guan@outlook.com>
2023-06-21 11:11:16 -0700

    6/21

--- algo.py
+++ algo.py
@@ -2814,3 +2814,23 @@ class Solution(object):
         if len(odd) == 0:
             return len(s)
         else:
+            return len(s) - len(odd) + 1
+        
+    def fractionToDecimal(self, numerator, denominator):
+
+        n, remainder = divmod(abs(numerator), abs(denominator))
+        sign = '-' if numerator*denominator < 0 else ''
+        result = [sign+str(n), '.']
+        remainders = {}
+
+        while remainder > 0 and remainder not in remainders:
+            remainders[remainder] = len(result)
+            n, remainder = divmod(remainder*10, abs(denominator))
+            result.append(str(n))
+
+        if remainder in remainders:
+            idx = remainders[remainder]
+            result.insert(idx, '(')
+            result.append(')')
+
+        return ''.join(result).rstrip(".")
