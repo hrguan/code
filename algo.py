69c0e31968303511970107b9f0108abe92c011dd
hrguan <huiru.guan@outlook.com>
2023-06-27 11:37:09 -0700

    6/27

--- algo.py
+++ algo.py
@@ -3190,4 +3190,34 @@ class NestedIterator(object):
                 res *= stack.pop()
                 res += stack.pop()
                 num = 0
-        return res + sign*nu
+        return res + sign*num
+
+    def calculate(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        num = 0
+        res = 0
+        pre_op = '+'
+        s+='+'
+        stack = []
+        for c in s:
+            if c.isdigit():
+                num = num*10 + int(c)
+            elif c == ' ':
+                    pass
+            else:
+                if pre_op == '+':
+                    stack.append(num)
+                elif pre_op == '-':
+                    stack.append(-num)
+                elif pre_op == '*':
+                    operant = stack.pop()
+                    stack.append((operant*num))
+                elif pre_op == '/':
+                    operant = stack.pop()
+                    stack.append(math.trunc(operant/num))
+                num = 0
+                pre_op = c
+        return sum(stack)
