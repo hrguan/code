52739e05aaadc100130e649f73382168c0e76ac5
hrguan <huiru.guan@outlook.com>
2023-06-27 11:15:24 -0700

    6/27

--- algo.py
+++ algo.py
@@ -3163,3 +3163,31 @@ class NestedIterator(object):
                     temp.append(l)
             return "".join(temp)
         return ""
+
+    def calculate(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        stack = []
+        num = 0
+        res = 0
+        sign = 1
+        for c in s:
+            if c.isdigit():
+                num = num*10+int(c)
+            elif c in "-+":
+                res += sign*num
+                sign = -1 if c =="-" else 1
+                num = 0
+            elif c == "(":
+                stack.append(res)  
+                stack.append(sign)
+                sign = 1
+                res = 0
+            elif c == ")":
+                res += sign*num
+                res *= stack.pop()
+                res += stack.pop()
+                num = 0
+        return res + sign*nu
