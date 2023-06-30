26576215024c370e5cbe738a14e51e557581022d
hrguan <huiru.guan@outlook.com>
2023-06-30 14:41:02 -0700

    6/30

--- algo.py
+++ algo.py
@@ -3511,3 +3511,21 @@ class OrderedStream(object):
             while self.pointer < len(self.arr) and self.arr[self.pointer] != None:
                 self.pointer += 1
             return self.arr[idKey:self.pointer]
+
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            m = (l+r) /2 
+            if nums[m] == target:
+                return m
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1
