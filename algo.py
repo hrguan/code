3e2539e2c6e57a5e1cc714f01997fb237dc757e1
hrguan <huiru.guan@outlook.com>
2023-07-12 22:58:28 -0700

    7/12

--- algo.py
+++ algo.py
@@ -4113,3 +4113,30 @@ class OrderedStream(object):
             right >>= 1
             i += 1
         return left << i
+
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        res = ""
+        i = len(a)-1
+        j = len(b)-1
+        carry = 0
+        while i >= 0 or j >= 0:
+            sum = carry
+            if i >= 0:
+                sum += (ord(a[i]) - ord("0"))
+            if j >= 0:
+                sum += (ord(b[j]) - ord("0"))
+            i -= 1
+            j -= 1
+            if sum > 1:
+                carry = 1
+            else:
+                carry = 0
+            res += str(sum%2)
+        if carry:
+            res += str(carry)
+        return res[::-1]
