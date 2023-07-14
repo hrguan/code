918b686f34d8976b303ffb439fdd8185fd454c18
hrguan <huiru.guan@outlook.com>
2023-07-13 20:43:44 -0700

    7/13

--- algo.py
+++ algo.py
@@ -4253,3 +4253,18 @@ class LFUCache(object):
             prev = node.val
             node = node.right
         return res
+
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+        return self.isMirror(root.left, root.right)
+    def isMirror(self, left, right):
+        if not left and not right:
+            return True
+        if not left or not right:
+            return False
+        return left.val == right.val and self.isMirror(left.left, right.right) and self.isMirror(left.right, right.left)
