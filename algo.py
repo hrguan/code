db86e33e90c1480a8faadcddc2a8d886bb119b1b
hrguan <huiru.guan@outlook.com>
2023-07-18 11:05:32 -0700

    7/18

--- algo.py
+++ algo.py
@@ -4677,3 +4677,14 @@ class Solution(object):
                     dfs(node.right, tmp+str(node.val)+"->")
         dfs(root, "")
         return res
+
+    def sumOfLeftLeaves(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        if root.left and not root.left.left and not root.left.right:
+            return root.left.val + self.sumOfLeftLeaves(root.right)
+        return self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
