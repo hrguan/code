84d5ec9d2b570fa061cb7403e28bbd21398efcc5
hrguan <huiru.guan@outlook.com>
2023-07-18 10:42:58 -0700

    7/`8

--- algo.py
+++ algo.py
@@ -4626,3 +4626,19 @@ class Solution(object):
                 dfs(root.right, sum-root.val, tmp+[root.val])
         dfs(root, targetSum, [])
         return res 
+
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        stack = []
+        stack.append(root)
+        while stack:
+            node = stack.pop()
+            if node:
+                res.append(node.val)
+                stack.append(node.right)
+                stack.append(node.left)
+        return res
