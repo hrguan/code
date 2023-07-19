cafdd10f64c3178bba5fe5186169832c8365beb0
hrguan <huiru.guan@outlook.com>
2023-07-19 09:45:00 -0700

    7/19

--- algo.py
+++ algo.py
@@ -4772,3 +4772,17 @@ class Solution(object):
                 dfs(child)
         dfs(root)
         return res
+
+    def mergeTrees(self, root1, root2):
+        """
+        :type root1: TreeNode
+        :type root2: TreeNode
+        :rtype: TreeNode
+        """
+        if root1 and root2:
+            root = TreeNode(root1.val+root2.val)
+            root.left = self.mergeTrees(root1.left, root2.left)
+            root.right = self.mergeTrees(root1.right, root2.right)
+            return root
+        else:
+            return root1 or root2
