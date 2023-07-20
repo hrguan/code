b9a983e5930b0502ac27466db20343fffd7b6038
hrguan <huiru.guan@outlook.com>
2023-07-20 09:57:29 -0700

    7/20

--- algo.py
+++ algo.py
@@ -4951,4 +4951,22 @@ class Solution(object):
             return dfs(node.left, res) + dfs(node.right, res)
         return dfs(root, 0)
     
+    def lexicalOrder(self, n):
+        """
+        :type n: int
+        :rtype: Lisst[int]
+        """
+        res = []
+        curr = 1
+        for i in range(n):
+            res.append(curr)
+            if curr * 10 <= n:
+                curr *= 10
+            else:
+                if curr >= n:
+                    curr //= 10
+                curr += 1
+                while curr % 10 == 0:
+                    curr //= 10
+        return res
     
