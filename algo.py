feb736b95099d42474468567302cd05f818b26e6
hrguan <huiru.guan@outlook.com>
2023-07-21 15:14:34 -0700

    7/21

--- algo.py
+++ algo.py
@@ -5012,3 +5012,17 @@ class Solution(object):
         for i in range(1, n+1):
             res.append(res[i >> 1] + i%2)
         return res
+
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        i = 0
+        j = 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+        return i == len(s)
