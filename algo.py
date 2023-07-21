c10c13aead1c5cd91706fa94bf51bd008f05e509
hrguan <huiru.guan@outlook.com>
2023-07-21 15:07:29 -0700

    7/21

--- algo.py
+++ algo.py
@@ -5002,3 +5002,13 @@ class Solution(object):
         :rtype: bool
         """
         return n % 2 == 0
+
+    def countBits(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        res = [0]
+        for i in range(1, n+1):
+            res.append(res[i >> 1] + i%2)
+        return res
