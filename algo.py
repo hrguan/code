3c3ce97d4450f7a01580507908dd4154a4309a53
hrguan <huiru.guan@outlook.com>
2023-07-24 10:54:54 -0700

    7/24

--- algo.py
+++ algo.py
@@ -5052,4 +5052,20 @@ class Solution(object):
             res = max(res, curr)
         return res
 
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        dp = [[0] * n for i in range(m)]
+        for i in range(0, n):
+            dp[0][i] = 1
+        for i in range(1, m):
+            dp[i][0] = 1
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[-1][-1]
+
     
