98bebc455ba469324da7ccabea7b8957934905b0
hrguan <huiru.guan@outlook.com>
2023-07-25 10:52:52 -0700

    7/25

--- algo.py
+++ algo.py
@@ -5114,3 +5114,20 @@ class Solution(object):
             for j in range(1, m):
                 dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1])
         return dp[-1][-1]
+
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s or s[0]== "0":
+            return 0
+        dp = [0] * (len(s)+1)
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, len(s)+1):
+            if 0 < int(s[i-1:i]) <= 9:
+                dp[i] += dp[i-1]
+            if 10 <= int(s[i-2:i]) <= 26:
+                dp[i] += dp[i-2]
+        return dp[-1]
