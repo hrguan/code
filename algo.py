6ee59465832ea120b9fe6e8166f5537da35666a7
hrguan <huiru.guan@outlook.com>
2023-07-27 16:28:31 -0700

    7/27

--- algo.py
+++ algo.py
@@ -5333,3 +5333,22 @@ class Solution(object):
             zero = one
             one = res
         return res
+
+    def getMaximumGenerated(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 0:
+            return 0
+        dp = [0] * (n+1)
+        dp[0] = 0
+        dp[1] = 1
+        
+        for i in range(2, n+1):
+            if i % 2 == 0:
+                dp[i] = dp[i//2]
+            else:
+                j = i //2
+                dp[i] = dp[j] + dp[j+1]
+        return max(dp)
