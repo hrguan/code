881eb0cae00db5f57e540b5aad2f1b8fe6a8d46d
hrguan <huiru.guan@outlook.com>
2023-07-31 11:04:30 -0700

    7/31

--- algo.py
+++ algo.py
@@ -5370,3 +5370,19 @@ class Solution(object):
                 sums[i] = arr[i] * (i+1)
             stack.append(i)
         return sum(sums) % (10**9+7)
+
+    def largestRectangleArea(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: int
+        """
+        heights.append(0)
+        res = 0
+        stack = [-1]
+        for i in range(len(heights)):
+            while stack and heights[i] < heights[stack[-1]]:
+                h = heights[stack.pop()]
+                w = i -stack[-1] -1
+                res = max(res, h*w)
+            stack.append(i)
+        return res
