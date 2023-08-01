5db6d470a7d0ae12690b66a235e60581532e4408
hrguan <huiru.guan@outlook.com>
2023-08-01 11:26:56 -0700

    8/1

--- algo.py
+++ algo.py
@@ -5450,7 +5450,7 @@ class Solution(object):
             bucketID = num // valueDiff
             if bucketID in buckets:
                 return True
-            for i in (bucketID-1, bucketID+1):
+            for  i in (bucketID-1, bucketID+1):
                 if i in buckets:
                     if abs(buckets[i] - num) < valueDiff:
                         return True
@@ -5458,3 +5458,28 @@ class Solution(object):
             if idx >= indexDiff:
                 del buckets[ nums[idx-indexDiff] // valueDiff]
         return False
+
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        cache = {}
+        def dfs(i, j):
+            if (i, j) in cache:
+                return cache[(i, j)]
+            if i >= len(s) and j >= len(p):
+                return True
+            if j >= len(p):
+                return False
+            match = i < len(s) and (s[i] == p[j] or p[j] == ".")
+            if (j+1) < len(p) and p[j+1] == "*":
+                cache[(i, j)] = dfs(i, j+2) or (match and dfs(i+1, j))
+                return cache[(i, j)]
+            if match:
+                cache[(i, j)] = dfs(i+1, j+1)
+                return cache[(i, j)]
+            cache[(i, j)] = False
+            return False
+        return dfs(0, 0)
