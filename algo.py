2a3f5cefd7d2d7b58041efe73cfae22b62ddf3e0
hrguan <huiru.guan@outlook.com>
2023-08-04 10:17:42 -0700

    8/4

--- algo.py
+++ algo.py
@@ -5611,4 +5611,18 @@ class Solution(object):
                 edit += 1
         return edit
 
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        for i in range(len(haystack) - len(needle)+1):
+            for j in range(len(needle)):
+                if haystack[i+j] != needle[j]:
+                    break
+                if j == len(needle)-1:
+                    return i
+        return -1
+
     
