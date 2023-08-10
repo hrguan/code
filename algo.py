3721da01d9368d3515fb6c9124a5a87f0d9770f7
hrguan <huiru.guan@outlook.com>
2023-08-10 14:42:58 -0700

    8/10

--- algo.py
+++ algo.py
@@ -5788,4 +5788,17 @@ class Solution(object):
             curr = curr.next
         return d[head]
 
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        res = ""
+        storeIntRoman = [[1000, "M"], [900, "CM"], [500, "D"], [400, "CD"], [100, "C"], [90, "XC"], [50, "L"], [40, "XL"], [10, "X"], [9, "IX"], [5, "V"], [4, "IV"], [1, "I"]]
+        for i in range(len(storeIntRoman)):
+            while num >= storeIntRoman[i][0]:
+                res += storeIntRoman[i][1]
+                num -= storeIntRoman[i][0]
+        return res
+
     
