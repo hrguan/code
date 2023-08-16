ef24eef4497a8a8d6e966deb111151af4819bd80
hrguan <huiru.guan@outlook.com>
2023-08-16 15:28:16 -0700

    8/16

--- algo.py
+++ algo.py
@@ -6043,3 +6043,14 @@ class Solution(object):
             minCur = min(num, minCur + num)
             minSum = min(minSum, minCur)
         return max(maxSum, total-minSum) if maxSum > 0 else maxSum
+
+
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        matrix.reverse()
+        for i in range(len(matrix)):
+            for j in range(i):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
