65a9a6406a93e317295ccae90f234960ad906d0b
hrguan <huiru.guan@outlook.com>
2023-08-17 09:45:34 -0700

    8/17

--- algo.py
+++ algo.py
@@ -6054,3 +6054,15 @@ class Solution(object):
         for i in range(len(matrix)):
             for j in range(i):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        l = 0
+        r = len(s)-1
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l+=1
+            r-=1
