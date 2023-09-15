58a4eaa76961314a163e08645bc4298691825a9e
hrguan <huiru.guan@outlook.com>
2023-09-15 09:58:20 -0700

    9/25

--- how2.py
+++ how2.py
@@ -1,6 +1,6 @@
 # 2.01 Array
 # 2.02 String
-# 2.03  Two Pointers
+# 2.03 Two Pointers
 # 2.04 Linked List
 # 2.13 Hash Table
 # 2.05 Stack
@@ -9,16 +9,17 @@
 2.17 Sliding Window
 
 2.06 Tree
-2.07 Dynamic Programming
-2.08 Backtracking
-2.09 Depth First Search
-2.10 Breadth First Search
-2.11 Binary Search
-
-
 2.18 Segment Tree
 2.19 Binary Indexed Tree
-2.16 Union Find
 
+2.11 Binary Search
+2.09 Depth First Search
+2.10 Breadth First Search
+
+2.07 Dynamic Programming
+2.08 Backtracking
+
+
+2.16 Union Find
 2.12 Math
 2.15 Bit Manipulation
