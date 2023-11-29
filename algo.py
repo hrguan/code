cefa7070b0c1b7c5e3a787b41fc0aa9ef8372291
hrguan <huiru.guan@outlook.com>
2023-11-28 20:33:56 -0800

    11/28

--- algo.py
+++ algo.py
@@ -6822,3 +6822,30 @@ class MyStack(object):
                 l+=1
             else:
                 r-=1
+
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        # space
+        # time
+        if len(nums) == 3:
+            if sum(nums) == 0:
+                return [nums]
+            return []
+        res = set()
+        nums.sort()
+        for i in range(0, len(nums)-2):
+            j = i+1
+            k = len(nums)-1
+            while j < k:
+                if nums[i]+nums[j]+nums[k] == 0:
+                    res.add((nums[i], nums[j], nums[k]))
+                    j += 1
+                    k -= 1
+                elif nums[i]+nums[j]+nums[k] < 0:
+                    j += 1
+                else:
+                    k -= 1
+        return res
