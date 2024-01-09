dcbab1e65ca6d22dd08ba7103be509e856b3ed25
hrguan <huiru.guan@outlook.com>
2024-01-08 21:13:39 -0800

    1/8

--- algo2.py
+++ algo2.py
@@ -1,1 +1,15 @@
-# starting at Nov. 2023
+# starting at Jan. 2024
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        prev = None
+        curr = head
+
+        while curr:
+            n = curr.next
+            curr.next = prev
+            prev = curr
+            curr = n
+        return prev
