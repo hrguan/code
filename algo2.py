aa2e3c37cbed9515f31403604e5f62c62f1eae6c
hrguan <huiru.guan@outlook.com>
2024-01-09 20:45:25 -0800

    1/9

--- algo2.py
+++ algo2.py
@@ -13,3 +13,16 @@
             prev = curr
             curr = n
         return prev
+
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
