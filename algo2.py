114d707d4059ea6d178b63a75d5e5ff3cb41a01a
hrguan <huiru.guan@outlook.com>
2024-01-11 21:07:38 -0800

    1/11

--- algo2.py
+++ algo2.py
@@ -54,3 +54,16 @@
             seen.add(head)
             head = head.next
         return False
+
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        seen = set()
+        while head:
+            if head in seen:
+                return head
+            seen.add(head)
+            head = head.next
+        return None
