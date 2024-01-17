78b85f32224cbb9be1b3dc7f4d02d6c78d2d9fe0
hrguan <huiru.guan@outlook.com>
2024-01-16 20:03:26 -0800

    1/16

--- algo2.py
+++ algo2.py
@@ -156,3 +156,17 @@
             if complementary in d:
                 return [d[complementary], i]
             d[nums[i]] = i
+    
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        res = set()
+        nums1 = set(nums1)
+        nums2 = set(nums2)
+        for num in nums2:
+            if num in nums1:
+                res.add(num)
+        return res
