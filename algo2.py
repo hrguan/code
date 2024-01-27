0da4ce60468889fe36c30642cd96e0dc909b2640
hrguan <huiru.guan@outlook.com>
2024-01-27 09:11:00 -0800

    1/27

--- algo2.py
+++ algo2.py
@@ -260,4 +260,25 @@
                 
         return str(bulls)+"A"+str(cows)+"B"
 
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        r_set = set()
+        c_set = set()
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+                if matrix[r][c] == 0:
+                    r_set.add(r)
+                    c_set.add(c)
+
+        
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+                if r in r_set or c in c_set:
+                    matrix[r][c] = 0
+
+        return matrix
+
 ############################################################
